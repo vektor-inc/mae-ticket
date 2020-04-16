d63ada4ae08019c8e38b04b86a9511dde356f5f8
naoki <n.globe.us@gmail.com>
2020-04-16 11:36:40 +0900

    一旦コミット

--- model/class.maetic_postmeta.php
+++ model/class.maetic_postmeta.php
@@ -3,4 +3,24 @@ class MaeTick_Postmeta {
     public static function init() {
 
     }
+	public static function is_orderId_valid($orderId){
+    	$order = get_post($orderId);
+		if(is_null($order)){
+			return;
+		};
+
+		$is_expired = MaeTick_Postmeta::is_orderId_expired($orderId);
+		$is_maeTick = MaeTick_Postmeta::is_orderId_maeTick($orderId);
+
+		if(!$is_expired && $is_maeTick){
+			return $order;
+		}
+	}
+
+	public static function is_orderId_expired($orderId){
+
+	}
+
+	public static function is_orderId_maeTick($orderId){
+	}
 }
