94bd9b3a5f639f4dd65f289d1c6099b16145abd9
naoki <n.globe.us@gmail.com>
2020-04-16 16:57:29 +0900

    一旦コミット

--- model/class.maetic_postmeta.php
+++ model/class.maetic_postmeta.php
@@ -3,24 +3,32 @@ class MaeTick_Postmeta {
     public static function init() {
 
     }
-	public static function is_orderId_valid($orderId){
-    	$order = get_post($orderId);
-		if(is_null($order)){
-			return;
-		};
 
-		$is_expired = MaeTick_Postmeta::is_orderId_expired($orderId);
-		$is_maeTick = MaeTick_Postmeta::is_orderId_maeTick($orderId);
-
-		if(!$is_expired && $is_maeTick){
-			return $order;
-		}
-	}
+//	public static function is_orderId_valid($orderId){
+//    	$order = get_post($orderId);
+//		if(is_null($order)){
+//			return false;
+//		};
+//
+//		$is_expired = MaeTick_Postmeta::is_orderId_expired($orderId);
+//		$is_maeTick = MaeTick_Postmeta::is_orderId_maetic_product($orderId);
+//
+//		return !$is_expired && $is_maeTick;
+//	}
 
 	public static function is_orderId_expired($orderId){
 
+		$_date_completed = get_post_meta($orderId, '_date_completed', true);
+		if($_date_completed === ''){
+			return false;
+		}
+		//unixtime
+		$expired_period =get_option( 'maetic_expired_period', false );
+		return time() <= $_date_completed + intval($expired_period);
 	}
 
-	public static function is_orderId_maeTick($orderId){
-	}
+//	public static function is_orderId_maetic_product($orderId){
+//		$is_maetic_product = get_post_meta($orderId, 'maetic_is_maetic_product', true);
+//		return $is_maetic_product !== '';
+//	}
 }
