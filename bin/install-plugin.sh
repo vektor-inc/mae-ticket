249c81ba434a03ca0e04790e4d1d50bc917fc4ae
naoki <n.globe.us@gmail.com>
2020-04-17 12:43:35 +0900

    テスト一旦削除

--- bin/install-plugin.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-export WP_PLUGIN=$(curl "https://api.wordpress.org/plugins/info/1.0/woocommerce.json" | jq -r .download_link)
-curl -s $WP_PLUGIN -o plugin.zip
-unzip plugin.zip -d .plugin
-rm -f plugin.zip
