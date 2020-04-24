f13aa8949db40f69b0bb21ba59579fc2d57e099f
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-04-24 14:19:32 +0900

    翻訳追加

--- mae-ticket.php
+++ mae-ticket.php
@@ -25,6 +25,8 @@ require_once( 'class.maetic_core.php' );
 require_once( 'class.maetic_admin.php' );
 
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	add_action( 'wp_enqueue_scripts', 'maetic_register_scripts' , 30, 0 );
+
 	MaeTick_Front_Controller::init();
 	MaeticCore::init();
 
@@ -33,4 +35,31 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	}
 }
 
+function maetic_register_scripts() {
+	wp_register_style(
+		'maetic',
+		plugins_url( 'assets/css/style.css', __FILE__ ),
+		array(),
+		'1',
+		'all'
+	);
+	wp_enqueue_style( 'maetic');
+
+	wp_register_script(
+		'maetic-form',
+		plugins_url( 'assets/js/form.min.js', __FILE__ ),
+		array(),
+		'1',
+		true
+	);
+	wp_enqueue_script( 'maetic-form' );
+}
+
+
+add_action(
+	'plugins_loaded',
+	function () {
+		load_plugin_textdomain( 'mae-ticket', false, '/mae-ticket/languages/' );
+	}
+);
 register_activation_hook( __FILE__, array( 'MaeTick_Front_Controller', 'set_rewrite_rules') );
