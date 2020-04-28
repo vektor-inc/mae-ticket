25911033fc0ca512dd6a3aacfcefc10843b5f241
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-04-28 15:49:00 +0900

    画面

--- class.maetic_admin.php
+++ class.maetic_admin.php
@@ -4,20 +4,65 @@ if ( !defined('ABSPATH') ) {
 	die();
 }
 
+
 class Maetic_Admin {
 	const WC_ORDER_POST_TYPE = 'shop_order';
 
 	public static function init() {
 		add_action( 'add_meta_boxes', array( __CLASS__, 'metabox_init' ) );
+		add_action( 'wp_dashboard_setup', array( __CLASS__, 'add_dashboard' ) );
+		add_action( 'admin_menu', array( __CLASS__, 'add_admin_page') );
+
 		add_filter( 'woocommerce_product_data_tabs', array( __CLASS__, 'product_tab'), 10, 1 );
 		add_action( 'woocommerce_product_data_panels', array( __CLASS__, 'product_tab_content'), 10, 0 );
 		add_action( 'woocommerce_process_product_meta', array( __CLASS__, 'save_product_options_field'),10, 1 );
 	}
 
+	public static function add_admin_page() {
+		add_submenu_page(
+			'woocommerce',
+			__( 'Maetic', 'textdomain' ),
+			__( 'Maetic', 'textdomain' ),
+			"administrator",
+			"maetic",
+			array( __CLASS__, 'admin_page' )
+		);
+	}
+
+	public static function admin_page() {
+		if ( isset($_GET[ 'action' ] ) ) {
+			if ( $_GET[ 'action' ] == 'url' ) {
+				MaeTick_Front_Controller::set_rewrite_rules();
+
+				?>
+<div id="maetic_noticebox">
+	<div class="notice notice-success settings-error is-dismissible">
+		<span><?php _e( 'parmalink updated.', 'mae-ticket' ); ?></span>
+	</div>
+</div>
+				<?php
+			}
+		}
+
+		include( dirname( __FILE__ ) . '/pages/admin_menu.php' );
+	}
+
+	public static function add_dashboard() {
+		wp_add_dashboard_widget(
+			'maetic_dashboard',
+			__( 'Maetic', 'mae-ticket' ),
+			array( __CLASS__, 'show_dashboard' )
+		);
+	}
+
+	public static function show_dashboard() {
+		include( dirname( __FILE__ ) . '/pages/dashboard.php' );
+	}
+
 	public static function product_tab( $tabs ) {
 		// Key should be exactly the same as in the class product_type
 		$tabs['simple'] = array(
-			'label'	 => __( 'Maetic', 'mae-ticket' ),
+			'label'  => __( 'Maetic', 'mae-ticket' ),
 			'target' => 'maetic_product_options',
 			'class'  => array( 'show_if_maetic_product' ),
 			);
@@ -32,21 +77,22 @@ class Maetic_Admin {
 		<?php
 			woocommerce_wp_checkbox(
 				array(
-					'id' 	=> 'enable_maetic_product',
+					'id'    => 'enable_maetic_product',
 					'label' => __( 'Enable Maetic Product', 'mae-ticket' ),
 				)
 			);
 
 			woocommerce_wp_text_input(
 				array(
-					'id'				=> 'maetic_expired_period',
-					'label'				=> __( 'Expired Period (Day)', 'mae-ticket' ),
-					'desc_tip'			=> 'true',
-					'description'		=> __( 'Enter the number of days the virtual ticket is valid for.', 'mae-ticket' ),
-					'type' 				=> 'number',
-					'custom_attributes'	=> array(
-						'min'	=> '1',
-						'step'	=> '1',
+					'id'                => 'maetic_expired_period',
+					'label'             => __( 'Expired Period (Day)', 'mae-ticket' ),
+					'desc_tip'          => 'true',
+					'description'       => __( 'Enter the number of days the virtual ticket is valid for.', 'mae-ticket' ),
+					'type'              => 'number',
+					'custom_attributes' => array(
+						'min'     => '1',
+						'step'    => '1',
+						'default' => 60
 					),
 				)
 			);
@@ -72,7 +118,7 @@ class Maetic_Admin {
 			array( __CLASS__, 'meta_box' ),
 			array( self::WC_ORDER_POST_TYPE ),
 			'normal',
-			'high',
+			'high'
 		);
 	}
 
